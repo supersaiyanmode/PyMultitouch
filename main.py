c965d7bcab362fb50445bab94ccf3e9bcc140fdb
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-06-11 23:46:35 -0700

    Added ability to detect 4 fingers, 3 fingers. Big fixes in gestures.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os.path
 import threading
 from subprocess import Popen, STDOUT, PIPE
+from Queue import Queue, Empty
+import signal
+from collections import Counter
+import re
 
 from pykeyboard import PyKeyboard
 
 NORTH, EAST, SOUTH, WEST = range(4)
-TIME, COORD, PRESSURE, FINGER = range(4)
+TIME, COORD, PRESSURE, FINGER, LEFT, RIGHT = range(6)
 
 class PyMTException(Exception):
     def __init__(self, msg):
@@ -20,7 +24,6 @@ class SynClientPoller(object):
     def __init__(self, pollFreq=50):
         self.command = self.POLL_COMMAND + [str(pollFreq)]
         self.listener = None
-        #self.thread = threading.Thread(target=self.run)
         self.stop_requested = False
 
     def register(self, obj):
@@ -28,19 +31,26 @@ class SynClientPoller(object):
         self.listener = obj
 
     def start(self):
-        self.thread.start()
-
-    def run(self):
         p = Popen(self.command, stdout=PIPE, stderr=PIPE, bufsize=1)
         while not self.stop_requested:
-            line = p.stdout.readline()
+            try:
+                line = p.stdout.readline()
+            except KeyboardInterrupt as e:
+                self.stop()
+                continue
             if not self.listener:
                 continue
+            print line.strip()
             data = self.parseData(str(line).strip())
             if data:
                 self.listener.event(data)
         p.stdout.close()
+        print "Closing client process."
         p.wait()
+        print "Closed client process."
+
+    def stop(self):
+        self.stop_requested = True
 
     def parseData(self, line):
         if line.startswith("time"):
@@ -52,57 +62,89 @@ class SynClientPoller(object):
             coords = (int(parts[1]), int(parts[2]))
             pressure = int(parts[3])
             fingers = int(parts[4])
-            return (time, coords, pressure, fingers)
+            left = bool(int(parts[6]))
+            right = bool(int(parts[7]))
+            return (time, coords, pressure, fingers, left, right)
         except Exception, e:
             print e
             return None
 
-class TouchpadState(object):
+class TouchpadEventProcessor(object):
     def __init__(self):
         self.listener = None
-        self.reset()
+        self.queue = Queue()
+        self.processorThread = threading.Thread(target=self.process)
+        self.processorThread.daemon = True
+        self.stop_requested = False
+
+    def start(self):
+        self.stop_requested = False
+        self.processorThread.start()
+
+    def stop(self):
+        self.stop_requested = True
 
     def register(self, obj):
         self.listener = obj
 
-    def reset(self):
-        self.history = []
-        self.prevFinger = 0
-        self.prevTime = -5
-
     def event(self, obj):
-        if obj[FINGER] == 0:
-            print "All fingers lifted. Reset."
-            self.process()
-            self.reset()
-            return
-
-        if obj[TIME] - self.prevTime > Config.TIME_THRESHOLD:
-            print "Time elapsed."
-            self.reset()
-
-        if obj[FINGER] != self.prevFinger:
-            self.reset()
-
-        self.prevFinger = obj[FINGER]
-        self.prevTime = obj[TIME]
-        self.history.append(obj)
+        self.queue.put(obj)
 
     def process(self):
-        if len(self.history) < 2:
+        print "Start processing events.."
+        history = []
+        prevTime = -Config.TIME_THRESHOLD
+        clicked = False
+        gesture = True
+        while not self.stop_requested:
+            item = self.queue.get()
+
+            self.queue.task_done()
+
+            if item[FINGER] == 0:
+                print "All fingers lifted."
+                if gesture:
+                    self.evaluate(history)
+                history = []
+                clicked = False
+                gesture = True
+                ignore = False
+            elif not clicked and (item[LEFT] or item[RIGHT]):
+                clicked = True
+                gesture = False
+                if self.listener:
+                    self.listener.click(LEFT if item[LEFT] else RIGHT, item[FINGER])
+            elif clicked and not (item[LEFT] or item[RIGHT]):
+                clicked = False
+            elif gesture:
+                history.append(item)
+        print "Done processing events."
+    
+    def evaluate(self, history):
+        if len(history) < 2:
             return
 
-        direction = self.get_direction([x[COORD] for x in self.history])
+        #finger = Counter(x[FINGER] for x in history).most_common(n=1)[0][0]
+        finger = max(x[FINGER] for x in history)
+
+        direction = self.get_direction([x[COORD] for x in history if x[FINGER] == finger])
+        if direction is None:
+            return
+
         print "Direction:", ["NORTH", "EAST", "SOUTH", "WEST"][direction],
-        print "Fingers:", self.prevFinger
+        print "Fingers:", finger
 
         if self.listener:
-            self.listener.event("SWIPE", direction, self.prevFinger)
+            self.listener.swipe(direction, finger)
         
 
     def get_direction(self, arr):
         arr = list(reversed(arr))
+        print "Arr:", arr
         delta = map(sum, zip(*[(x[0] - y[0], x[1] - y[1]) for x, y in zip(arr, arr[1:])]))
+        print "Delta:", delta
+        if len(delta) != 2:
+            return None
         if abs(delta[0]) < abs(delta[1]): #More difference in Y direction
             return NORTH if delta[1] < 0 else SOUTH
         else:
@@ -133,45 +175,79 @@ class KeyMapper(object):
                 if len(parts) != 2:
                     raise PyMTException("Invalid syntax near: " + line)
                 key, value = map(lambda x: x.strip(), parts)
-                combinations = map(lambda x: x.strip(), value.split("+"))
-                if any([x not in self.KEY_MAP for x in combinations]):
-                    raise PyMTException("Invalid key combination: " + line)
-                key_combos = [getattr(self.keyboard, self.KEY_MAP[c]) for c in combinations]
-                self.map[key] = (key_combos, value)
+                if value:
+                    combinations = map(lambda x: x.strip(), value.split("+"))
+                    key_combos = [self.parse_key(c) for c in combinations]
+                    self.map[key] = (key_combos, value)
+                else:
+                    self.map[key] = []
                 print "Loaded:", line
+    
+    def parse_key(self, key):
+        if key in self.KEY_MAP:
+            return getattr(self.keyboard, self.KEY_MAP[key])
+        elif key.isalpha():
+            return key.lower()
+        elif re.match('[fF]((1[0-2]?)|[2-9])', key):
+            return self.keyboard.function_keys[int(key[1:])]
+        else:
+            raise PyMTException("Invalid key combination: " + line)
+        
 
-    def event(self, style, direction, fingers):
-        style = "SWIPE"
+    def swipe(self, direction, fingers):
         direction = ["NORTH", "EAST", "SOUTH", "WEST"][direction]
         fingers = str(fingers) + "_FINGERS"
 
-        map_key = "%s_%s_%s"%(style, direction, fingers)
+        map_key = "SWIPE_%s_%s"%(direction, fingers)
+        self.process_event(map_key)
 
-        if map_key not in self.map:
+    def click(self, typ, fingers):
+        typ = "LEFT" if typ == LEFT else RIGHT
+        map_key = "%s_CLICK_%d_FINGERS"%(typ, fingers)
+        self.process_event(map_key)
+    
+    def process_event(self, map_key):
+        if map_key not in self.map or not self.map[map_key]:
             return
 
         combinations, text = self.map[map_key]
 
         print "Sending:", text
+        if len(combinations) > 1:
+            for press_key in combinations[:-1]:
+                self.keyboard.press_key(press_key)
+            self.keyboard.tap_key(combinations[-1])
 
-        for press_key in combinations[:-1]:
-            self.keyboard.press_key(press_key)
+            for press_key in combinations[:-1]:
+                self.keyboard.release_key(press_key)
+        else:
+            self.keyboard.tap_key(combinations[0])
 
-        self.keyboard.tap_key(combinations[-1])
+poller = SynClientPoller()
+touchpad = TouchpadEventProcessor()
+keymapper = KeyMapper()
 
-
-        for press_key in combinations[:-1]:
-            self.keyboard.release_key(press_key)
-
-
+def exit():
+    print "Stopping .."
+    poller.stop()
+    touchpad.stop()
 
 def main():
-    poller = SynClientPoller()
-    touchpad = TouchpadState()
-    keymapper = KeyMapper()
+    for sig in (signal.SIGTERM, signal.SIGINT):
+        prev_handler = signal.getsignal(sig)
+
+        def sig_handler(x, y, prevFn):
+            exit()
+            if prevFn:
+                prevFn(x, y)
+        signal.signal(sig, lambda x, y: sig_handler(x, y, prev_handler))
+    
+
     poller.register(touchpad)
     touchpad.register(keymapper)
-    poller.run()
+    touchpad.start()
+    poller.start()
+
 
 if __name__ == '__main__':
     main()
